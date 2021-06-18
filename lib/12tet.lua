d439749fe75104fd2c2c794fc267481bba212a2f
Leonora Tindall <nora@nora.codes>
2021-06-19 04:31:53 +0700

    fix: midi output erroring, thanks @xmacex

--- lib/12tet.lua
+++ lib/12tet.lua
@@ -7,7 +7,7 @@ tuning.NOTE_A4 = 69
 tuning.TWELFTH_ROOT_OF_2 = 2^(1/12)
 
 function tuning.midi_note_to_freq(note)
-  return FREQ_A4 * (TWELFTH_ROOT_OF_2 ^ (note - NOTE_A4))
+  return tuning.FREQ_A4 * (tuning.TWELFTH_ROOT_OF_2 ^ (note - tuning.NOTE_A4))
 end
 
 return tuning
